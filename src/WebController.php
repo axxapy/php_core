d51086af61304853dc889d7f5e0bbaa4e3b6840b
Alexey Manukhin <axxapy@gmail.com>
2017-01-19 14:34:47 -0800

    add WebController with request object

---
+++ src/WebController.php
@@ -0,0 +1,18 @@
+<?php namespace axxapy;
+
+use axxapy\Web\WebRequest;
+
+class WebController extends Controller {
+	/** @var WebRequest */
+	private $Request;
+
+	/**
+	 * @return \axxapy\Web\WebRequest
+	 */
+	public function getRequest(): \axxapy\Web\WebRequest {
+		if (!$this->Request) {
+			$this->Request = new WebRequest();
+		}
+		return $this->Request;
+	}
+}
